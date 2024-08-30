3d388eb851a47c399c4d7aa227684f9f8a426cfa
Yours01amit <emmysingh019@gmail.com>
2024-08-31 01:53:00 +0530

    prediction pipeline completed

--- src/utils.py
+++ src/utils.py
@@ -18,8 +18,8 @@ def save_object(file_path, obj):
     
 def load_object(file_path):
     try:
-        with open(file_path, "rb") as file_objt:
-            return pickle.load(file_objt)
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
 
